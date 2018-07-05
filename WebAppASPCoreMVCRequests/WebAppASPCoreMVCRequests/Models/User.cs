0e6c07341190f8ae7066503ab8b4f6b1b12b3e51
PavloPaitak <38991419+PavloPaitak@users.noreply.github.com>
2018-07-05 22:34:07 +0300

    Created class User

---
+++ WebAppASPCoreMVCRequests/WebAppASPCoreMVCRequests/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace WebAppASPCoreMVCRequests.Models
+{
+    public sealed class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Avatar { get; set; }
+        public string Email { get; set; }
+        public string CreatedAt { get; set; }
+
+        public List<Post> Posts { get; set; }
+        public List<Todo> Todos { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}; Name:{Name}; Avatar:{Avatar}; Email:{Email}; CreatedAt:{CreatedAt}";
+        }
+    }
+}
