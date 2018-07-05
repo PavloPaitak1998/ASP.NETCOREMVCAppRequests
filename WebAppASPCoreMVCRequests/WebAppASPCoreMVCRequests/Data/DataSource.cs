ae2958143d701be4bc3d857a385dc381ff8ff287
PavloPaitak <38991419+PavloPaitak@users.noreply.github.com>
2018-07-05 22:47:35 +0300

    Created class DataSource

---
+++ WebAppASPCoreMVCRequests/WebAppASPCoreMVCRequests/Data/DataSource.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using WebAppASPCoreMVCRequests.Models;
+
+namespace WebAppASPCoreMVCRequests.Data
+{
+    public class DataSource
+    {
+        public List<User> Users { get; set; }
+        public List<Post> Posts { get; set; }
+        public List<Comment> Comments { get; set; }
+        public List<Todo> Todos { get; set; }
+    }
+}
