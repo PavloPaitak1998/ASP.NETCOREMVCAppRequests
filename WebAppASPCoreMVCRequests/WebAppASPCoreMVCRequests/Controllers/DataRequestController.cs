c41116417f108c219255c539270e9188034855c5
PavloPaitak <38991419+PavloPaitak@users.noreply.github.com>
2018-07-07 13:30:44 +0300

    Created a validation for requests(uncorrect input of id)

--- WebAppASPCoreMVCRequests/WebAppASPCoreMVCRequests/Controllers/DataRequestController.cs
+++ WebAppASPCoreMVCRequests/WebAppASPCoreMVCRequests/Controllers/DataRequestController.cs
@@ -14,9 +14,9 @@ namespace WebAppASPCoreMVCRequests.Controllers
             dataSource = _dataSource;
         }
 
-        public IActionResult Info()
+        public IActionResult Info(string actionId, string message)
         {
-            return View();
+            return View((actionId,message));
         }
 
         public IActionResult GetAllData()
@@ -26,6 +26,11 @@ namespace WebAppASPCoreMVCRequests.Controllers
 
         public IActionResult GetCommentsCount(int id)
         {
+            if (!dataSource.Users.Exists(u=>u.Id==id))
+            {
+                return Redirect($"~/DataRequest/Info?actionId=1&message=User with this id {id} doesn't exist !");
+            }
+
             var commentsCount = LinqRequests.CommentsCount(id,dataSource.Posts);
 
             return View(commentsCount);
@@ -33,12 +38,22 @@ namespace WebAppASPCoreMVCRequests.Controllers
 
         public IActionResult GetUserComments(int id)
         {
+            if (!dataSource.Users.Exists(u => u.Id == id))
+            {
+                return Redirect($"~/DataRequest/Info?actionId=2&message=User with this id {id} doesn't exist !");
+            }
+
             var res = LinqRequests.GetUserComments(id, dataSource.Posts);
             return View(res);
         }
 
         public IActionResult GetUserTodos(int id)
         {
+            if (!dataSource.Users.Exists(u => u.Id == id))
+            {
+                return Redirect($"~/DataRequest/Info?actionId=3&message=User with this id {id} doesn't exist !");
+            }
+
             var res = LinqRequests.GetUserTodos(id, dataSource.Todos);
             return View(res);
         }
@@ -52,6 +67,11 @@ namespace WebAppASPCoreMVCRequests.Controllers
 
         public IActionResult GetAdditionalUserInfo(int id)
         {
+            if (!dataSource.Users.Exists(u => u.Id == id))
+            {
+                return Redirect($"~/DataRequest/Info?actionId=5&message=User with this id {id} doesn't exist !");
+            }
+
             var res = LinqRequests.GetAdditionalUserInfo(id, dataSource.Users);
 
             return View(res);
@@ -59,6 +79,11 @@ namespace WebAppASPCoreMVCRequests.Controllers
 
         public IActionResult GetAdditionalPostInfo(int id)
         {
+            if (!dataSource.Posts.Exists(p => p.Id == id))
+            {
+                return Redirect($"~/DataRequest/Info?actionId=6&message=Post with this id {id} doesn't exist !");
+            }
+
             var res = LinqRequests.GetAdditionalPostInfo(id, dataSource.Posts);
 
             return View(res);
